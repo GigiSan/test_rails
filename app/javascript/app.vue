c705252da0a0d8a8e9f8761424cf168361a93784
Alessandro Grigis <alegrigis@gmail.com>
2024-05-15 11:56:08 +0000

    feat(home): added post search input field

--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,27 +1,88 @@
 <template>
   <div id="app">
+    <input type="text" v-model="searchTerm" placeholder="Cerca per titolo o tag..." />
+    <Spinner v-if="loading" />
     <post v-for="post in posts" :key="post.id" :post="post"></post>
+    <p v-if="noPostsFound">Nessun post trovato!</p>
+    <p v-if="fetchError">Non è stato possibile caricare i post, riprova più tardi.</p>
   </div>
 </template>
 
 <script>
 import Post from './components/Post'
+import Spinner from './components/Spinner';
+
+const debounce = function(fn, wait) {
+  let timer;
+  return function (...args) {
+    if (timer) {
+      clearTimeout(timer);
+    }
+    const context = this;
+    timer = setTimeout(() => {
+      fn.apply(context, args);
+    }, wait);
+  }
+}
 
 export default {
-  components: { Post },
+  components: { Post, Spinner },
 
   data() {
     return {
-      posts: []
+      searchTerm: "",
+      posts: [],
+      loading: true,
+      fetchError: false
     }
   },
 
+  created() {
+    this.debouncedFetch = debounce((term) => {
+      fetch(`/api/posts/search/${term}`)
+        .then(response => response.json())
+        .then(data => {
+          this.posts = data;
+          this.fetchError = false;
+        })
+        .catch(_error => {
+          this.fetchError = true;
+          this.posts = []
+        })
+        .finally(() => {
+          this.loading = false
+        })
+    }, 600);
+  },
+
   mounted() {
+    this.loading = true
     fetch('/api/posts')
     .then(response => response.json())
     .then(data => {
       this.posts = data;
+      this.fetchError = false;
     })
+    .catch(_error => {
+      this.fetchError = true;
+      this.posts = []
+    })
+    .finally(() => {
+      this.loading = false
+    })
+  },
+
+  computed: {
+    noPostsFound: function() {
+      return this.posts.length === 0 && !this.fetchError
+    }
+  },
+
+  watch: {
+    searchTerm(newVal) {
+      this.loading = true
+      this.debouncedFetch(newVal);
+    }
   }
 }
 </script>
@@ -30,5 +91,12 @@ export default {
 p {
   font-size: 2em;
   text-align: center;
+}
+
+input {
+  margin: 0.5em 2em;
+  width: 100%;
+  max-width: 300px;
+  font-size: 1em;
 }
 </style>
---
+++ app/javascript/components/Spinner.vue
@@ -0,0 +1,31 @@
+<template>
+  <svg width="24" height="24" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+    <circle class="spinner_S1WN" cx="4" cy="12" r="3"/>
+    <circle class="spinner_S1WN spinner_Km9P" cx="12" cy="12" r="3"/>
+    <circle class="spinner_S1WN spinner_JApP" cx="20" cy="12" r="3"/>
+  </svg>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style scoped>
+    .spinner_S1WN {
+      animation: spinner_MGfb .8s linear infinite;
+      animation-delay: -.8s
+    }
+    .spinner_Km9P {
+      animation-delay: -.65s
+    }
+    .spinner_JApP {
+      animation-delay: -.5s
+    }
+    @keyframes spinner_MGfb {
+      93.75%,
+      100% {
+        opacity: .2
+      }
+    }
+</style>
